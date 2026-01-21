d57dd93df781a9f91cd96f217700f7db705b6952
David Helkowski <github@codechild.com>
2026-01-21 12:49:54 +0900

    Slim repo to Apple-only tools

--- model/model.cpp
+++
@@ -1,4 +0,0 @@
-#include "model.h"
-
-std::shared_ptr<model_info> models::unknown = std::make_shared<model_unknown>();
-std::shared_ptr<model_info> models::largest = std::make_shared<model_rp_generic>();
