7f1b7588b671d187368e1f721bcc90f5a49649a4
nanoscopic <nanoscopic@users.noreply.github.com>
2026-01-21 15:13:43 +0000

    Update Homebrew formula for 0.1.5

--- Formula/dapico-tools.rb
+++ Formula/dapico-tools.rb
@@ -1,8 +1,8 @@
 class DapicoTools < Formula
   desc "macOS USB tools for RP2040-based boards"
   homepage "https://github.com/nanoscopic/dapico-tools"
-  url "https://github.com/nanoscopic/dapico-tools/archive/refs/tags/0.1.4.tar.gz"
-  sha256 "b39fa950f4080d5702ab00bc77ccc3c3ce057dc1b8963a1e3496607f0faccb29"
+  url "https://github.com/nanoscopic/dapico-tools/archive/refs/tags/0.1.5.tar.gz"
+  sha256 "bda2f6b18a429a2d3a6b54801542688b2fabf5fbb0bcd6b27af92dee8ea9670c"
   license "BSD-3-Clause"
   depends_on "cmake" => :build
   depends_on :macos
