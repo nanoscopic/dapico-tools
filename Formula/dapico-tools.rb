fde910bb1ea73d02cd757d66b65f4659b8c1e162
David Helkowski <github@codechild.com>
2026-01-21 16:44:07 +0900

    Add workflow to update Homebrew formula

--- Formula/dapico-tools.rb
+++ Formula/dapico-tools.rb
@@ -1,11 +1,10 @@
 class DapicoTools < Formula
-  desc "macOS-only USB tools for Raspberry Pi Pico devices"
+  desc "macOS USB tools for RP2040-based boards"
   homepage "https://github.com/nanoscopic/dapico-tools"
   url "https://github.com/nanoscopic/dapico-tools/archive/refs/tags/0.1.2.tar.gz"
   sha256 "860fac5a0ca4e26f78bb894d9ceb18e82ce290c9268f5f62790f4ab564459835"
-  version "0.1.2"
+  license "BSD-3-Clause"
   depends_on "cmake" => :build
-  depends_on :macos
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
