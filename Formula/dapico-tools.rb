6e2e937fe3f10d65eb40930228d312e9ef0de371
nanoscopic <nanoscopic@users.noreply.github.com>
2026-01-21 15:47:28 +0000

    Update Homebrew formula for 0.1.6

--- Formula/dapico-tools.rb
+++ Formula/dapico-tools.rb
@@ -1,8 +1,8 @@
 class DapicoTools < Formula
   desc "macOS USB tools for RP2040-based boards"
   homepage "https://github.com/nanoscopic/dapico-tools"
-  url "https://github.com/nanoscopic/dapico-tools/archive/refs/tags/0.1.5.tar.gz"
-  sha256 "bda2f6b18a429a2d3a6b54801542688b2fabf5fbb0bcd6b27af92dee8ea9670c"
+  url "https://github.com/nanoscopic/dapico-tools/archive/refs/tags/0.1.6.tar.gz"
+  sha256 "e8cad6d3ec49b3428de91f87c38c47e0bce9a475658b5274f8edfcda2d553437"
   license "BSD-3-Clause"
   depends_on "cmake" => :build
   depends_on :macos
@@ -19,13 +19,10 @@ class DapicoTools < Formula
   end
 
   test do
-    return unless OS.mac?
-
     system "#{bin}/dapico-reboot", "--help"
     system "#{bin}/dapico-load", "--help"
     testpath.install resource("test-elf")
-    output = shell_output("#{bin}/dapico-load --dryrun #{testpath}/test.elf 2>&1")
-    puts "dapico-load output:\n#{output}"
+    output = shell_output("#{bin}/dapico-load --dryrun #{testpath}/test.elf")
     assert_match "write RAM 0x20000000 (33500 bytes", output
     assert_match "write RAM 0x200082dc (5832 bytes", output
     assert_match "execute at 0x200001e9", output
